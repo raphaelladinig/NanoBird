8b5c918bc443e34430bc68acd79dc381965f04a2
Raphael Ladinig <raphaelladinig07@gmail.com>
2024-05-27 09:33:18 +0200

    bt

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,23 @@
 
 int b1 = 2;
 int b2 = 3;
+
 SoftwareSerial bt(10, 0);
+struct Signal {
+    char id;
+    int value;
+};
+char btChar = ' ';
+char btId = ' ';
+char btIdTemp = ' ';
+bool btIdExpected = true;
+bool btNewReading = false;
+String btValueString = "";
+float btValue;
+Signal btQuery();
+
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(10, 13, NEO_GRB + NEO_KHZ800);
+
 Adafruit_SSD1306 display(128, 64, &Wire);
 
 void setup() {
@@ -22,6 +37,34 @@ void setup() {
     Serial.println("Started");
 }
 
-void loop() {
-     display.display();
+void loop() { display.display(); }
+
+Signal btQuery() {
+    Signal result = {0, 0};
+
+    btNewReading = false;
+    if (bt.available()) {
+        btChar = bt.read();
+        if (btIdExpected) {
+            btIdTemp = btChar;
+            btIdExpected = false;
+            btValueString = "";
+        } else {
+            if ((btChar != ' ') && (btChar != '\n')) {
+                btValueString += btChar;
+            }
+            if (btChar == '\n') {
+                result.id = btIdTemp;
+                result.value = btValueString.toFloat();
+                Serial.print(result.id);
+                Serial.print(" ");
+                Serial.println(result.value);
+                btValueString = "";
+                btIdExpected = true;
+                btNewReading = true;
+            }
+        }
+    }
+
+    return result;
 }
