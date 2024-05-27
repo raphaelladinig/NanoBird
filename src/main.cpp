4b2b02a3d968df26028fe4da88bb65fabeaa3df5
Raphael Ladinig <raphaelladinig07@gmail.com>
2024-05-27 09:15:44 +0200

    test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,27 @@
+#include "Adafruit_NeoPixel.h"
+#include "Adafruit_SSD1306.h"
+#include "SoftwareSerial.h"
 #include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
 
-void setup() {}
+int b1 = 2;
+int b2 = 3;
+SoftwareSerial bt(10, 0);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(10, 13, NEO_GRB + NEO_KHZ800);
+Adafruit_SSD1306 display(128, 64, &Wire);
 
-void loop() {}
+void setup() {
+    pinMode(b1, INPUT);
+    pinMode(b2, INPUT);
+
+    bt.begin(9600);
+    Serial.begin(9600);
+    strip.begin();
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+    Serial.println("Started");
+}
+
+void loop() {
+     display.display();
+}
